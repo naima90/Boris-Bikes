4ad729ada29e12b3710f4ba425396f2d59e95fe7
Naima <naimaosman203@gmail.com>
2021-12-09 15:28:04 +0000

    step 11 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,11 @@
 require 'docking_station'
 describe DockingStation do
+    docking_station = DockingStation.new
+    bike = Bike.new
     it {expect(DockingStation.new).to respond_to :release_bike} 
-    it {expect(DockingStation.new.release_bike.working?).to be true}
-    
+    it {expect(DockingStation.new.release_bike.working?).to be true} 
+    it {expect(DockingStation.new).to respond_to :dock}
+    expect(docking_station.dock(bike)).to eq bike
+    expect(docking_station.bike).to eq bike
 end
+
